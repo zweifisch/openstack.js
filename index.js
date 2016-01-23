d9b23cc5b64b85ad412d341eea8de02cc189930f
Feng Zhou <zf.pascal@gmail.com>
2016-01-23 14:48:10 +0800

    more modules

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ exports.authenticate = require("./lib/openstack").authenticate;
 exports.nova = require("./lib/nova");
 exports.keystone = require("./lib/keystone");
 exports.gnocchi = require("./lib/gnocchi");
+exports.cinder = require("./lib/cinder");
+exports.searchlight = require("./lib/searchlight");
