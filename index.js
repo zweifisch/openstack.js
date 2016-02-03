89c87b445f0b12fbccf47c3a6728f7660df92886
Feng Zhou <zf.pascal@gmail.com>
2016-02-03 21:00:09 +0800

    neutron

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "use strict";
 
 exports.authenticate = require("./lib/openstack").authenticate;
+
 exports.nova = require("./lib/nova");
+exports.neutron = require("./lib/neutron");
 exports.keystone = require("./lib/keystone");
 exports.gnocchi = require("./lib/gnocchi");
 exports.cinder = require("./lib/cinder");
+exports.heat = require("./lib/heat");
 exports.searchlight = require("./lib/searchlight");
+
+exports.defres = require("./lib/util").defres;
