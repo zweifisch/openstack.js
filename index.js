70d6756bc639569b9a672e313c0d46366bce0a86
Feng Zhou <zf.pascal@gmail.com>
2016-04-16 14:34:54 +0800

    add swift

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ exports.manila = require("./lib/manila");
 exports.ironic = require("./lib/ironic");
 exports.searchlight = require("./lib/searchlight");
 exports.ceilometer = require("./lib/ceilometer");
+exports.swift = require("./lib/swift");
 
 exports.defres = require("./lib/util").defres;
 exports.endpoint = require("./lib/util").endpoint;
